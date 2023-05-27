ed15037f3ea494fef5efd5b1711b4e01c4be6061
dagmara <dagmara.gabriela.stach@gmail.com>
2023-05-27 16:58:20 +0200

    scripts added

---
+++ scripts.js
@@ -0,0 +1,66 @@
+/* -----click on offer on the top Navbar -> scroll to offer content on the page ------*/
+document.getElementById('offerNavbar').addEventListener('click', () => {
+    window.scrollTo(0, document.getElementById('offer').offsetTop);
+});
+/* -----click on contact on the top Navbar -> scroll to contact content on the page ------*/
+document.getElementById('contactNavbar').addEventListener('click', () => {
+    window.scrollTo(0, document.getElementById('contact').offsetTop);
+});
+
+/* -----click on about Me on the top Navbar -> scroll to AboutMe content on the page ------*/
+document.getElementById('aboutMeNavbar').addEventListener('click', () => {
+    window.scrollTo(0, document.getElementById('AboutMe').offsetTop);
+});
+
+/* ----Function - scroll page to the top - position (0,0) ---- */
+let aArrowIcon = document.getElementById("aArrowIcon");
+aArrowIcon.onclick = function () {
+    scrollTo(0, 0)
+};
+
+
+function myFunction() {
+    var x = document.getElementById("navBarContainer");
+    if (x.className === "navBarContainer") {
+        x.className += " responsive";
+    } else {
+        x.className = "navBarContainer";
+    }
+}
+
+const scrollPhoto = (counter) => {
+    const imgElement = document.getElementById('photoSlideShow');
+    const imgNumber = imgElement.getAttribute('src').replace('img/', '').replace('.webp', '');
+    console.log(imgElement.getAttribute('src'));
+    const minImgNumber = 1;
+    const maxImgNumber = 3;
+    /*
+            const imgNumber = imgElement.getAttribute('src').replace('img/', '').replace('.jpg', '');
+    */
+    let nextImgNumber = Number(imgNumber) + counter;
+    nextImgNumber = nextImgNumber === minImgNumber - 1 ? maxImgNumber : nextImgNumber;
+    nextImgNumber = nextImgNumber === maxImgNumber + 1 ? minImgNumber : nextImgNumber;
+    /*
+            document.getElementById('photoSlideShow').setAttribute('src', `img/${nextImgNumber}.jpg`);
+    */
+    document.getElementById('photoSlideShow').setAttribute('src', `img/${nextImgNumber}.webp`);
+
+};
+
+var a = document.getElementById("file");
+Object.values(a.files).forEach((el) => {
+    console.log(el.name);
+});
+let selFilesContainer = document.getElementById("selectedFiles");
+a.addEventListener('change', (selectedFiles) => {
+    selectedFiles = "";
+    Object.values(a.files).forEach((el) => {
+        console.log(el.name);
+        let s = document.createElement('span');
+        s.innerHTML = el.name;
+        s.className = 'fileText';
+        let br = document.createElement('br');
+        selFilesContainer.appendChild(s);
+        selFilesContainer.appendChild(br);
+    });
+});
